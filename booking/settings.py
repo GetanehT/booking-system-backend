e523f70d56215618a18d12fb1440fafdcb7070c6
Getaneh Biratu <mrgetaneh@gmail.com>
2024-10-21 22:07:45 +0000

    fix secretkey

--- booking/settings.py
+++ booking/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-k$##t-%6uge!*#j7y^h=wr@jj)dfsd)=9%z69v8^5rz&area_)"
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -86,6 +87,7 @@ CORS_ALLOW_ALL_ORIGINS = True
    #     "NAME": BASE_DIR / "db.sqlite3",
     #}
 #}
+
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
